a35e1dd79700390ceb77be31e3a5401804c80280
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-03-13 17:39:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+const body=document.getElementsByTagName("body")[0];
+body.style="background-color:blue";
+
+
+
+const h1=document.createElement("h1");
+h1.textContent=prompt("Ism kiriting");
+h1.style="text-align:center; font-size:50px; margin-top:250px; color:yellow";
+
+body.appendChild(h1);
